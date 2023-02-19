eed90e8266e7db3f82579031668e0f28f91c5df6
Reinir <blackwhitenxt@gmail.com>
2023-02-19 10:56:13 +0700

    nothing to comment here

--- src/CDHouse/model/CD.java
+++ src/CDHouse/model/CD.java
@@ -6,88 +6,58 @@ import java.io.Serializable;
  *
  * @author NguyenXuanTrung QE170172
  */
-public abstract class CD implements Serializable {
-    private String collectionName = "";
-    private String title = "";
-    private double price = 0.0;
-    private String id = "";
+public class CD implements Serializable {
+
+    public String collectionName = "";
+    public String type = "";
+    public String title = "";
+    public double price = 0.0;
+    public String id = "";
+    public String pubYear = "";
 
     public CD() {
     }
 
-
-    // "\" before "~" and "\"
-    static private String stuffing(String basic) {
-        String result = "";
-        for (int i = 0; i < basic.length(); ++i) {
-            if (basic.charAt(i) == '~' || basic.charAt(i) == '\\') {
-                result += '\\';
-            }
-            result += basic.charAt(i);
-        }
-        return result;
-    }
-
-    static private String destuffing(String basic) {
-        String result = "";
-        for (int i = 0; i < basic.length(); ++i) {
-            if (basic.charAt(i) == '\\') {
-
-                if (i + 1 > basic.length()
-                        || basic.charAt(i + 1) != '~' && basic.charAt(i + 1) != '\\') {
-                    throw new RuntimeException("Invalid stuffed string");
-                }
-                continue;
-            }
-            result += basic.charAt(i);
-        }
-        return result;
-    }
-
-    public final String getCollectionName() {
-        return destuffing(collectionName);
-    }
-    public final void setCollectionName(String collectionName) {
-        this.collectionName = stuffing(collectionName);
-    }
-
-    public final String getTitle() {
-        return destuffing(title);
-    }
-
-    public final void setTitle(String title) {
-        this.title = stuffing(title);
-    }
-
-    public final double getPrice() {
-        return price;
-    }
-
-    public final void setPrice(double price) {
+    public CD(String id,
+            String title,
+            String pubYear,
+            String collectionName,
+            String type,
+            double price) {
+        this.id = id;
+        this.title = title;
+        this.pubYear = pubYear;
+        this.collectionName = collectionName;
+        this.type = type;
         this.price = price;
     }
 
-    public final String getId() {
-        return destuffing(id);
-    }
-
-    public final void setId(String id) {
-        this.id = stuffing(id);
-    }
-
-    public CD(String collectionName, String title, double price, String id) {
-        setCollectionName(collectionName);
-        setTitle(title);
-        setPrice(price);
-        setId(id);
+    public CD(String CD) {
+        String[] tokens = CD.split(" - ");
+        try {
+            if (tokens.length != 6) {
+                throw new RuntimeException();
+            }
+            this.id = tokens[0];
+            this.title = tokens[1];
+            this.pubYear = tokens[2];
+            this.collectionName = tokens[3];
+            this.type = tokens[4];
+            this.price = Double.parseDouble(tokens[5]);
+        } catch (Exception ex) {
+            throw new RuntimeException("Invalid CD");
+        }
     }
 
     //Use "~" as seperator
     @Override
     public String toString() {
-        return collectionName + "~" + title + "~" + price + "~" + id;
+        return id + " - "
+                + title + " - "
+                + pubYear + " - "
+                + collectionName + " - "
+                + type + " - "
+                + price;
     }
-    public abstract void play();
 
-    public abstract String getType();
 }
--- src/CDHouse/model/Catalog.java
+++ src/CDHouse/model/Catalog.java
@@ -1,33 +1,73 @@
 package CDHouse.model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.function.Predicate;
+
 /**
  *
- * @author NguyenXuanTrung
+ * @author NguyenXuanTrung QE170172
  */
-public class Catalog {
-    private CD[] cdList;
-    private int size;
-    private int maxSize = 10;
-
-    public Catalog() {
-        cdList = new CD[maxSize];
-        size = 0;
+public final class Catalog extends DynamicArray<CD> implements Serializable {
+    public Catalog(int capacity) {
+        super(capacity);
     }
 
-    private boolean bigger() {
-        CD[] cdListn = new CD[maxSize * 2];
-        maxSize = maxSize * 2;
-        for (int i = 0; i < size; ++i) {
-            cdListn[i] = cdList[i];
+    public Catalog() {
+        super(10);
+    }
+
+    public boolean saveToFile(String filename) {
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(filename));
+            os.writeObject(this);
+        } catch (Exception ex) {
+            return false;
         }
-        cdList = cdListn;
         return true;
     }
 
-    public boolean add(CD n) {
-        if (size + 1 >= maxSize && bigger() == false) {
-
+    public boolean takeFromFile(String filename) {
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream(filename));
+            Catalog x = (Catalog) is.readObject();
+            return this.shallowCopy(x);
+        } catch (Exception e) {
+            return false;
         }
+    }
+
+    public boolean idExisted(String id) {
+        for (int i = 0; i < this.size(); ++i) {
+            if (this.get(i).id.toLowerCase().equals(id.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public Catalog search(Predicate<CD> x) {
+        Catalog res = new Catalog();
+
+        for (int i = 0; i < this.size(); ++i) {
+            if (x.test(this.get(i))) {
+                res.add(this.get(i));
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        String s = "CD list:\n";
+        for (int i = 0; i < this.size(); ++i) {
+            s += this.get(i) + "\n";
+        }
+        return s;
     }
 
 }
