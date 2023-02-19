871e003a6188076c81e7db870ab3b0e719b01e98
Reinir <blackwhitenxt@gmail.com>
2023-02-19 10:56:53 +0700

    nothing to comment here

--- src/CDHouse/CDManagement.java
+++ src/CDHouse/CDManagement.java
@@ -4,9 +4,6 @@
  */
 package CDHouse;
 
-import CDHouse.model.CDFactory;
-import CDHouse.model.CD;
-import CDHouse.model.VideoCD;
 import CDHouse.control.Controller;
 
 /**
@@ -15,30 +12,6 @@ import CDHouse.control.Controller;
  */
 public class CDManagement {
 
-    public static void CD_construct_test() {
-        CD x = new VideoCD();
-        x.setCollectionName("Game");
-        x.setTitle("Hollow Knight");
-        x.setId("1");
-        x.setPrice(12);
-        assert (x.toString().equals("Game~Hollow Knight~12.0~1"));
-
-        x.setTitle("Hollow~Knight");
-        assert (x.toString().equals("Game~Hollow\\~Knight~12.0~1"));
-
-        x.setId("\\ARID");
-        assert (x.toString().equals("Game~Hollow\\~Knight~12.0~\\\\ARID"));
-
-        assert (x.getTitle().equals("Hollow~Knight"));
-        assert (x.getId().equals("\\ARID"));
-
-        x.setTitle("Hollow~Knight\\~\\");
-
-        CD retype = CDFactory.parse(x.toString());
-        assert (retype.getTitle().equals("Hollow~Knight\\~\\"));
-        retype.play();
-
-    }
     public static void main(String[] args) {
         Controller control = new Controller();
         control.run();
